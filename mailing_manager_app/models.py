1ae71f94f3f1d6ff61f2a5b281ce71b1e41c3720
Denis Ivanov <ledorubden@gmail.com>
2022-04-26 20:05:53 +0300

    Create models.

---
+++ mailing_manager_app/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Recipient(models.Model):
+    phone_number = models.CharField(
+        max_length=10,
+        unique=True
+    )
+    country_code = models.CharField(max_length=3)
+    tag = models.CharField(max_length=50, blank=True, null=True)
+    timezone = models.CharField(max_length=50)
+
+
+class Mailing(models.Model):
+    start_time = models.DateTimeField()
+    stop_time = models.DateTimeField()
+    msg = models.TextField()
+    filters = models.JSONField()
+
+
+class Message(models.Model):
+    date_sent = models.DateTimeField()
+
+    class StatusChoices(models.TextChoices):
+        PENDING = 'pending'
+        SENT = 'sent'
+        CANCELLED = 'canceled'
+
+    status = models.CharField(
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING,
+        max_length=50
+    )
+
+    mailing = models.ForeignKey(
+        'Mailing',
+        on_delete=models.CASCADE,
+        related_name='mailing'
+    )
+
+    recipient = models.ForeignKey(
+        'Recipient',
+        on_delete=models.CASCADE,
+        related_name='recipient'
+    )
---
+++ mailing_manager_project/secret.py
@@ -0,0 +1,2 @@
+DB_USER_NAME = 'django'
+DB_USER_PASS = 'dja'
