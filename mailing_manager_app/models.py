c35ac0dee9853fbafe2d313f067119d200d698e7
Denis Ivanov <ledorubden@gmail.com>
2022-04-26 23:53:06 +0300

    Fix phone number considered unique regardless of the country code.

--- mailing_manager_app/models.py
+++ mailing_manager_app/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 class Recipient(models.Model):
     phone_number = models.CharField(
-        max_length=10,
-        unique=True
+        max_length=10
     )
     country_code = models.CharField(max_length=3)
     tag = models.CharField(max_length=50, blank=True, null=True)
     timezone = models.CharField(max_length=50)
+
+    class Meta:
+        unique_together = ('country_code', 'phone_number')
 
 
 class Mailing(models.Model):
